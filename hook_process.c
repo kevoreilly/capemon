234e4a9e8001af8f8e94e20611766b715dc4a9d1
Kevin O'Reilly <kevoreilly@gmail.com>
2021-05-28 20:06:43 +0100

    Remove instruction-based filtering in ntdll write-protection

--- hook_process.c
+++ hook_process.c
@@ -1013,23 +1013,18 @@ HOOKDEF(BOOLEAN, WINAPI, RtlDispatchException,
 			if (dllname && !strcmp(dllname, "ntdll.dll")) {
 				free(dllname);
 				// if trying to write to ntdll.dll, then just skip the instruction
-				_DecodedInst instruction;
 #ifdef _WIN64
-				if (ide(&instruction, (void*)Context->Rip) && !stricmp("mov", instruction.mnemonic.p)) {
-					if (!ntdll_protect_logged) {
-						ntdll_protect_logged = TRUE;
-						DebugOutput("RtlDispatchException: skipped %s instruction at 0x%x writing to ntdll (0x%x - 0x%x)\n", instruction.mnemonic.p, Context->Rip, ExceptionRecord->ExceptionInformation[1], offset);
-					}
-					Context->Rip += lde((void*)Context->Rip);
+				if (!ntdll_protect_logged) {
+					ntdll_protect_logged = TRUE;
+					DebugOutput("RtlDispatchException: skipped instruction at 0x%x writing to ntdll (0x%x - 0x%x)\n", Context->Rip, ExceptionRecord->ExceptionInformation[1], offset);
 				}
+				Context->Rip += lde((void*)Context->Rip);
 #else
-				if (ide(&instruction, (void*)Context->Eip) && !stricmp("mov", instruction.mnemonic.p)) {
-					if (!ntdll_protect_logged) {
-						ntdll_protect_logged = TRUE;
-						DebugOutput("RtlDispatchException: skipped %s instruction at 0x%x writing to ntdll (0x%x - 0x%x)\n", instruction.mnemonic.p, Context->Eip, ExceptionRecord->ExceptionInformation[1], offset);
-					}
-					Context->Eip += lde((void*)Context->Eip);
+				if (!ntdll_protect_logged) {
+					ntdll_protect_logged = TRUE;
+					DebugOutput("RtlDispatchException: skipped instruction at 0x%x writing to ntdll (0x%x - 0x%x)\n", Context->Eip, ExceptionRecord->ExceptionInformation[1], offset);
 				}
+				Context->Eip += lde((void*)Context->Eip);
 #endif
 				return TRUE;
 			}
