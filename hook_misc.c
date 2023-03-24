655dc0f40d44247357a03d616199412fb7920dbe
Kevin O'Reilly <kevoreilly@gmail.com>
2023-03-24 12:40:51 +0000

    Syscall hooks: protect ProcessInstrumentationCallback via NtSetInformationProcess hook

--- hook_misc.c
+++ hook_misc.c
@@ -720,7 +720,9 @@ HOOKDEF(NTSTATUS, WINAPI, NtSetInformationProcess,
 	__in PVOID ProcessInformation,
 	__in ULONG ProcessInformationLength
 ) {
-	NTSTATUS ret = Old_NtSetInformationProcess(ProcessHandle, ProcessInformationClass, ProcessInformation, ProcessInformationLength);
+	NTSTATUS ret = 0;
+	if (!g_config.syscall || ProcessInformationClass != ProcessInstrumentationCallback)
+		ret = Old_NtSetInformationProcess(ProcessHandle, ProcessInformationClass, ProcessInformation, ProcessInformationLength);
 	if ((ProcessInformationClass == ProcessExecuteFlags || ProcessInformationClass == ProcessBreakOnTermination) && ProcessInformationLength == 4)
 		LOQ_ntstatus("process", "ii", "ProcessInformationClass", ProcessInformationClass, "ProcessInformation", *(int*)ProcessInformation);
 	else
