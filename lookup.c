e64136052b2957e40504ab8feae8c8ec6c9f22cf
Kevin O'Reilly <kevoreilly@gmail.com>
2023-06-13 19:28:20 +0100

    Fix issue with Microsoft Edge (64-bit) not launching properly

--- lookup.c
+++ lookup.c
@@ -16,6 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "ntapi.h"
 #include "lookup.h"
 
 void lookup_free(lookup_t *d)
