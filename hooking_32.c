kevoreilly/capemon

commit 610e9e342558007b23763182a8a7cf2bde0a50cc

Hooking: remove fallback to GetExportAddress() when GetProcAddress() fails (e.g. 12094a47a9659b1c2f7c5b36e21d2b0145c9e7b2e79845a437508efa96e5f305)

hooking_32.c
@@ -703,10 +703,6 @@ int hook_api(hook_t *h, int type)
 				char *module_name = convert_address_to_dll_name_and_offset((ULONG_PTR)addr, &offset);
 				DebugOutput("hook_api: Warning - %s export address 0x%p differs from GetProcAddress -> 0x%p (%s::0x%x)\n", h->funcname, exportaddr, addr, module_name, offset);
 			}
-			else if (exportaddr && !addr) {
-				addr = exportaddr;
-				DebugOutput("hook_api: %s address 0x%p obtained via GetExportAddress\n", h->funcname, addr);
-			}
 		}
 
 		if (addr == NULL && h->timestamp != 0 && h->rva != 0) {

hooking_64.c
@@ -968,10 +968,6 @@ int hook_api(hook_t *h, int type)
 				char *module_name = convert_address_to_dll_name_and_offset((ULONG_PTR)addr, &offset);
 				DebugOutput("hook_api: Warning - %s export address 0x%p differs from GetProcAddress -> 0x%p (%s::0x%x)\n", h->funcname, exportaddr, addr, module_name, offset);
 			}
-			else if (exportaddr && !addr) {
-				addr = exportaddr;
-				DebugOutput("hook_api: %s address 0x%p obtained via GetExportAddress\n", h->funcname, addr);
-			}
 		}
 
 		if (addr == NULL && h->timestamp != 0 && h->rva != 0) {

commit f105698b081d0866095431bd600008b1ecee0a33

Fix hooking of .NET compileMethod() using GetExportAddress() (broken in 610e9e3)

hooking_32.c
@@ -703,6 +703,10 @@ int hook_api(hook_t *h, int type)
 				char *module_name = convert_address_to_dll_name_and_offset((ULONG_PTR)addr, &offset);
 				DebugOutput("hook_api: Warning - %s export address 0x%p differs from GetProcAddress -> 0x%p (%s::0x%x)\n", h->funcname, exportaddr, addr, module_name, offset);
 			}
+			else if (exportaddr && !addr && !wcscmp(h->library, L"clrjit")) {
+				addr = exportaddr;
+				DebugOutput("hook_api: clrjit::%s export address 0x%p obtained via GetExportAddress\n", h->funcname, addr);
+			}
 		}
 
 		if (addr == NULL && h->timestamp != 0 && h->rva != 0) {

hooking_64.c
@@ -968,6 +968,10 @@ int hook_api(hook_t *h, int type)
 				char *module_name = convert_address_to_dll_name_and_offset((ULONG_PTR)addr, &offset);
 				DebugOutput("hook_api: Warning - %s export address 0x%p differs from GetProcAddress -> 0x%p (%s::0x%x)\n", h->funcname, exportaddr, addr, module_name, offset);
 			}
+			else if (exportaddr && !addr && !wcscmp(h->library, L"clrjit")) {
+				addr = exportaddr;
+				DebugOutput("hook_api: clrjit::%s export address 0x%p obtained via GetExportAddress\n", h->funcname, addr);
+			}
 		}
 
 		if (addr == NULL && h->timestamp != 0 && h->rva != 0) {