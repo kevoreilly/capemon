2d465bcd41c163af5f046280ce32c452c13df11b
Kevin O'Reilly <kevoreilly@gmail.com>
2024-06-04 21:31:43 +0100

    hooking_64: Refine check for native hook type (Win7)

--- hooking_64.c
+++ hooking_64.c
@@ -1030,7 +1030,7 @@ int hook_api(hook_t *h, int type)
 
 	addr = handle_stub(h, addr);
 
-	if (h->library && !wcscmp(h->library, L"ntdll") && !memcmp(addr, "\x4c\x8b\xd1\xb8", 4)) {
+	if (!wcscmp(h->library, L"ntdll") && !memcmp(addr, "\x4c\x8b\xd1\xb8", 4) && memcmp(addr+8, "\x0f\x05", 2)) {
 		// hooking a native API, leave in the mov eax, <syscall nr> instruction
 		// as some malware depends on this for direct syscalls
 		// missing a few syscalls is better than crashing and getting no information
