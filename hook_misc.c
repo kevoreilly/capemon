d4781c064f0ac159a702b94da395cccb8e84bbde
mhdo <mhdo@umass.edu>
2025-08-22 09:52:17 -0400

    Changed according to comments

--- hook_misc.c
+++ hook_misc.c
@@ -26,6 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "hook_sleep.h"
 #include "config.h"
 #include "ignore.h"
+#include "powerbase.h"
 #include "CAPE\CAPE.h"
 #include "CAPE\Injection.h"
 #include "CAPE\Debugger.h"
@@ -1960,4 +1961,19 @@ HOOKDEF(ULONG, __fastcall, vDbgPrintExWithPrefixInternal,
 	DebugOutput("%s", Buffer);
 
     return Old_vDbgPrintExWithPrefixInternal(Prefix, ComponentId, Level, Format, arglist, HandleBreakpoint);
+}
+
+HOOKDEF(BOOL, WINAPI, GetPwrCapabilities,
+	_Out_	PSYSTEM_POWER_CAPABILITIES lpspc
+) {
+	BOOL ret = Old_GetPwrCapabilities(lpspc);
+	if (ret) {
+		// Most VM systems does not support either S0 or S3 sleep, which can be used to detect the presence of a VM.
+		// S0, S4 and S5 being enabled is typical for a normal Modern Standby machine. 
+		(*lpspc).AoAc = 1;
+		(*lpspc).SystemS4 = 1;
+		(*lpspc).SystemS5 = 1;
+	}
+	LOQ_bool("device", "");
+	return ret;
 }
