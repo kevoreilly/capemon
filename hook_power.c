d4aa77055fc8435ce3dc4531480aea991e12059d
mhdo <mhdo@umass.edu>
2025-08-22 12:07:22 +0700

    Update hook_power.c for more accurate anti-anti-vm measures
    Most Windows VMs don't support either S0 or S3 sleep out of the box, which can be used as an anti-VM measure.

--- hook_power.c
+++ hook_power.c
@@ -9,7 +9,13 @@ HOOKDEF(BOOL, WINAPI, GetPwrCapabilities,
 	_Out_	PSYSTEM_POWER_CAPABILITIES lpspc
 ){
 	BOOL ret = Old_GetPwrCapabilities(lpspc);
-	(*lpspc).SystemS4 = 1;
-	LOQ_bool("device", "iiii", "S1", lpspc->SystemS1, "S2", lpspc->SystemS2, "S3", lpspc->SystemS3, "S4", lpspc->SystemS4);
+	if (ret) { 
+		// Most VM systems does not support either S0 or S3 sleep, which can be used to detect the presence of a VM.
+		// S0, S4 and S5 being enabled is typical for a normal Modern Standby machine. 
+		(*lpspc).SystemAoAc = 1;
+		(*lpspc).SystemS4 = 1;
+		(*lpspc).SystemS5 = 1;
+	}
+	LOQ_bool("device", "");
 	return ret;
 }
