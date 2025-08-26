8307b5037ff370df505630f331fe1b282879a9f4
mhdo <mhdo@umass.edu>
2025-08-26 14:16:00 -0400

    Update hook_misc.c

--- hook_misc.c
+++ hook_misc.c
@@ -1967,12 +1967,13 @@ HOOKDEF(BOOL, WINAPI, GetPwrCapabilities,
 	_Out_	PSYSTEM_POWER_CAPABILITIES lpspc
 ) {
 	BOOL ret = Old_GetPwrCapabilities(lpspc);
-	if (ret) {
+	if (ret && lpspc) {
 		// Most VM systems does not support either S0 or S3 sleep, which can be used to detect the presence of a VM.
 		// S0, S4 and S5 being enabled is typical for a normal Modern Standby machine. 
 		(*lpspc).AoAc = 1;
 		(*lpspc).SystemS4 = 1;
 		(*lpspc).SystemS5 = 1;
+		(*lpspc).ThermalControl = 1;
 	}
 	LOQ_bool("device", "");
 	return ret;
