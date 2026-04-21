7e91547e3a4d95ca67d6d201b68fc15e81078885
cccs-mog <117194682+cccs-mog@users.noreply.github.com>
2026-04-21 16:52:36 -0400

    Update hook_wmi.c

--- hook_wmi.c
+++ hook_wmi.c
@@ -83,6 +83,10 @@ void SpoofWmiData(const wchar_t* szClassName, const wchar_t* wszName, VARIANT* p
 			}
 		}
 	}
+	else if (pVal->vt == VT_BOOL) {
+		if ((!_wcsicmp(szClassName, L"Win32_ComputerSystem") && (!_wcsicmp(wszName, L"PartOfDomain"))
+			pVal->boolVal = True;
+	}
 	//
 	// Spoofery logic for NULL
 	//
