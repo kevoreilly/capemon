f1a54c0ace86041044d8f7527982439144178959
William Metcalf <wmetcalf@users.noreply.github.com>
2026-05-21 13:07:10 -0700

    Scylla: Remove ATL dependency in StringConversion using native Win32 APIs

--- CAPE/Scylla/StringConversion.cpp
+++ CAPE/Scylla/StringConversion.cpp
@@ -1,22 +1,53 @@
 #include "StringConversion.h"
-//#include <cstdlib>
-#include <atlbase.h> 
-#include <atlconv.h>
+#include <windows.h>
+#include <cstring>
 
 const char* StringConversion::ToASCII(const wchar_t* str, char* buf, size_t bufsize)
 {
-	//wcstombs(buf, str, bufsize);
-	ATL::CW2A str_a = str;
-	strncpy_s(buf, bufsize, str_a, bufsize);
+	if (!buf || bufsize == 0) {
+		return buf;
+	}
+
+	if (!str) {
+		buf[0] = '\0';
+		return buf;
+	}
+
+	size_t len = 0;
+	while (str[len] && len < bufsize - 1) len++;
+
+	int written = WideCharToMultiByte(CP_UTF8, 0, str, (int)len, buf, (int)bufsize - 1, NULL, NULL);
+	if (written >= 0) {
+		buf[written] = '\0';
+	} else {
+		buf[0] = '\0';
+	}
+
 	buf[bufsize - 1] = '\0';
 	return buf;
 }
 
 const wchar_t* StringConversion::ToUTF16(const char* str, wchar_t* buf, size_t bufsize)
 {
-	//mbstowcs_s(buf, str, bufsize);
-	ATL::CA2W str_w = str;
-	wcsncpy_s(buf, bufsize, str_w, bufsize);
+	if (!buf || bufsize == 0) {
+		return buf;
+	}
+
+	if (!str) {
+		buf[0] = L'\0';
+		return buf;
+	}
+
+	size_t len = 0;
+	while (str[len] && len < bufsize - 1) len++;
+
+	int written = MultiByteToWideChar(CP_UTF8, 0, str, (int)len, buf, (int)bufsize - 1);
+	if (written >= 0) {
+		buf[written] = L'\0';
+	} else {
+		buf[0] = L'\0';
+	}
+
 	buf[bufsize - 1] = L'\0';
 	return buf;
 }
