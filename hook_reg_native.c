7e3ea3f817ecc3e4f20fc3aca6180fb9a3597426
doomedraven <doommedraven@gmail.com>
2026-08-20 12:23:36 +0200

    Define STATUS_NO_MORE_ENTRIES in hook_reg_native.c to restore compilation

--- hook_reg_native.c
+++ hook_reg_native.c
@@ -18,6 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdio.h>
 #include "ntapi.h"
+
+#define STATUS_NO_MORE_ENTRIES ((NTSTATUS)0x8000001AL)
 #include "hooking.h"
 #include "log.h"
 #include "pipe.h"
